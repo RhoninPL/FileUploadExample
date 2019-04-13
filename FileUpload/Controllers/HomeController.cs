8bcb5978d603b8d17614aae5b6d78707d924d1e4
Michał Świtalik <michal.switalik@gmail.com>
2019-04-13 17:16:05 +0200

    Initial commit

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FileUpload.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        [HttpPost]
+        public IActionResult Upload(FileUploadViewModel model)
+        {
+            var file = model.File;
+
+            using (var fileStream = new FileStream($"{_env.WebRootPath}/images/{file.FileName}", FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ FileUpload/Models/FileUploadViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FileUpload.Models
+{
+    public class FileUploadViewModel
+    {
+        public IFormFile File { get; set; }
+
+        public long Size { get; set; }
+    }
+}
